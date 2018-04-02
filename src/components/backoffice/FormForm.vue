8f43c983f93f9524dd4e965a754ec58157a539d0
Coen Mooij <coenmooij@gmail.com>
2018-04-02 21:52:00 +0200

    fixup! [WIP] add form management

--- src/components/backoffice/FormForm.vue
+++ src/components/backoffice/FormForm.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="card">
+  <div
+    v-if="formsLoaded" 
+    class="card">
     <h4 class="card-header">Add form to developer</h4>
     <div class="card-body">
       <div 
@@ -19,6 +21,15 @@
           </option>
         </select>  
       </div>
+      <div 
+        v-if="showQuestions && questionsLoaded"
+        class="mb-3">
+        <app-question
+          v-for="question in form.questions"
+          :question="question"
+          :key="question.id" />
+      </div>
+      <app-loading-animation v-if="showQuestions && !questionsLoaded" />
       <div class="action-bar">
         <button 
           class="btn btn-danger"
@@ -31,31 +42,72 @@
           @click="select">
           <i class="fa fa-plus" /> Add to developer
         </button>
+        <button
+          v-if="showQuestions"
+          class="btn btn-success"
+          @click="submit">
+          <i class="fa fa-arrow-right" /> Submit form
+        </button>
       </div>
     </div>
   </div>
+  <app-loading-animation v-else />
 </template>
 
 <script>
+import LoadingAnimation from "../layout/LoadingAnimation";
+import FormQuestion from "./FormQuestion";
+
 export default {
+  components: {
+    "app-loading-animation": LoadingAnimation,
+    "app-question": FormQuestion
+  },
   data() {
     return {
       formId: null,
       showQuestions: false,
-      forms: []
+      questionsLoaded: false,
+      formsLoaded: false,
+      forms: [],
+      form: null,
+      answers: []
     };
+  },
+  created() {
+    this.getForms();
   },
   methods: {
     cancel() {
       this.$emit("cancel");
     },
     select() {
+      this.getQuestions(this.formId);
       this.showQuestions = true;
     },
     submit() {
       this.showQuestions = false;
       this.formId = null;
       this.$emit("submit");
+    },
+    getQuestions(id) {
+      this.$http
+        .get("forms/developers/" + id)
+        .then(response => response.json())
+        .then(data => {
+          this.form = data.data.form;
+          this.questionsLoaded = true;
+        });
+    },
+    getForms() {
+      this.$http
+        .get("forms/developers")
+        .then(response => response.json())
+        .then(data => {
+          this.forms = data.data.forms;
+          this.formId = this.forms[0].id;
+          this.formsLoaded = true;
+        });
     }
   }
 };
---
+++ src/components/backoffice/FormQuestion.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form-group">
+    <label :for="'question'+ question.id">
+      {{ question.value }}
+    </label>
+    <input 
+      v-model="answer"
+      type="text"
+      class="form-control">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: {
+      type: Object,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      answer: this.value
+    };
+  },
+  watch: {
+    answer(value) {
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
